e8b1d92dfb5a435ac62548b826f54a6fc11da58c
Thomas West <thom8750@stud.kea.dk>
2020-09-30 11:05:11 +0200

    Controller

--- src/main/java/com/madspild/madspild/controller/MyController.java
+++ src/main/java/com/madspild/madspild/controller/MyController.java
@@ -1,9 +1,12 @@
 package com.madspild.madspild.controller;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class MyController {
 
     @GetMapping("/")
